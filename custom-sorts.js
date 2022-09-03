ab753891b283a97c1a21ffce9eb05747b8063419
Samuel Park <s.park111497@gmail.com>
2022-09-03 00:02:15 -0700

    finished assignment

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,21 +1,39 @@
 function ageSort(users) {
-  // Your code here
+  users.sort((a, b) => a.age - b.age);
+  return users;
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  arr.sort((a, b) => {
+    if (a % 2 === 1 && b % 2 === 0) return -1;
+    if (a % 2 === 0 && b % 2 === 1) return 1;
+    return a - b;
+  });
+  return arr
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  let sortedS = s.split('').sort()
+    , sortedT = t.split('').sort();
+  return sortedS.every((el, index) => el === sortedT[index]);
+
 }
 
 function reverseBaseSort(arr) {
-  // Your code here
+  arr.sort((a, b) => {
+    if (a.toString().length > b.toString().length) return -1;
+    if (a.toString().length < b.toString().length) return 1;
+    return a - b;
+  });
+  return arr
 }
 
 function frequencySort(arr) {
-  // Your code here
+  let frequencyMap = new Object();
+  for (let i = 0; i < arr.length; i++) {
+    frequencyMap[arr[i]] = (frequencyMap[arr[i]] || 0) + 1;
+  };
+  return arr.sort((a, b) => frequencyMap[a] - frequencyMap[b] || b - a);
 }
 
 module.exports = [
